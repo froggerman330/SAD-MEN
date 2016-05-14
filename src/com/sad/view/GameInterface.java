d7bc7b161ed67133e7656788ad34467e8ac529d1
Devon <froggerman330@gmail.com>
2016-05-14 17:36:04 +1000

    Updated default board to show rows/cols.

--- src/com/sad/view/GameInterface.java
+++ src/com/sad/view/GameInterface.java
@@ -4,8 +4,8 @@ import com.sad.data.Move;
 
 public class GameInterface
 {
-    private String defaultBoard = "o-----o-----o\n" + "| o---o---o |\n" + "| | o-o-o | |\n" + "o-o-o   o-o-o\n"
-            + "| | o-o-o | |\n" + "| o---o---o |\n" + "o-----o-----o";
+    private static String defaultBoard = "1 2 3 4 5 6 7" + "1 o-----o-----o\n" + "2 | o---o---o |\n"
+            + "3 | | o-o-o | |\n" + "4 o-o-o   o-o-o\n" + "5 | | o-o-o | |\n" + "6 | o---o---o |\n" + "7 o-----o-----o";
 
     public void displayError(Exception e)
     {
