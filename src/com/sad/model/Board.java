b1186c996e149c83de0b67f1d0e3c331eaf44187
Devon <froggerman330@gmail.com>
2016-05-19 09:30:18 +1000

    Fixed a couple of mill formed detection rows.

--- src/com/sad/model/Board.java
+++ src/com/sad/model/Board.java
@@ -70,9 +70,9 @@ public class Board
 		this.millMap.put(new int[] { 3, 0 }, new int[][][] { { { 3, 1 }, { 3, 2 } }, { { 0, 0 }, { 6, 0 } } });
 		this.millMap.put(new int[] { 3, 1 }, new int[][][] { { { 3, 0 }, { 3, 2 } }, { { 1, 1 }, { 5, 1 } } });
 		this.millMap.put(new int[] { 3, 2 }, new int[][][] { { { 3, 0 }, { 3, 1 } }, { { 2, 2 }, { 4, 2 } } });
-		this.millMap.put(new int[] { 3, 4 }, new int[][][] { { { 3, 5 }, { 3, 6 } }, { { 0, 6 }, { 6, 6 } } });
+		this.millMap.put(new int[] { 3, 4 }, new int[][][] { { { 3, 5 }, { 3, 6 } }, { { 2, 4 }, { 4, 4 } } });
 		this.millMap.put(new int[] { 3, 5 }, new int[][][] { { { 3, 4 }, { 3, 6 } }, { { 1, 5 }, { 5, 5 } } });
-		this.millMap.put(new int[] { 3, 6 }, new int[][][] { { { 3, 4 }, { 3, 5 } }, { { 2, 4 }, { 4, 4 } } });
+		this.millMap.put(new int[] { 3, 6 }, new int[][][] { { { 3, 4 }, { 3, 5 } }, { { 0, 6 }, { 6, 6 } } });
 		this.millMap.put(new int[] { 4, 2 }, new int[][][] { { { 4, 3 }, { 4, 4 } }, { { 2, 2 }, { 3, 2 } } });
 		this.millMap.put(new int[] { 4, 3 }, new int[][][] { { { 4, 2 }, { 4, 4 } }, { { 5, 3 }, { 6, 3 } } });
 		this.millMap.put(new int[] { 4, 4 }, new int[][][] { { { 4, 2 }, { 4, 3 } }, { { 2, 4 }, { 3, 4 } } });
